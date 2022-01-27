90df6e599d34c904270e74d01785ef9d3e33e046
Code Palace <codepalace@fastmail.se>
2022-01-27 16:07:20 +0100

    Initial Commit

---
+++ SwipeableTabs/SwipeableTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableTabsApp.swift
+//  SwipeableTabs
+//
+//  Created by Federico on 27/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
